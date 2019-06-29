e22fed6324c2b62a5bac9aa2c17ef8943276d042
DingkeXue <605534423@qq.com>
2019-06-29 16:02:27 +0800

    模块化模式

---
+++ 模块化模式/main.js
@@ -0,0 +1,26 @@
+/*
+* 我们可以通过在一个单一的对象中包含共有/私有的方法和变量，从而使全局范围中屏蔽特定的部分
+* 这个结果可以减少对全局变量的污染
+* */
+
+/*
+* 例1
+* */
+let testModule = (function () {
+    let counter = 0;
+
+    return {
+        incrementCounter: function () {
+            return counter++;
+        },
+
+        resetCounter: function () {
+            console.log('counter value prior to reset:' + counter);
+            counter = 0;
+        }
+    }
+})();
+
+// usage
+testModule.incrementCounter();
+testModule.resetCounter();
